b214dc13bee09779e24d05933fbc8bde21b562be
Mitchell Rosen <mitchellwrosen@gmail.com>
2017-05-18 23:11:24 -0400

    Some work on server, and fix up some types

--- server/haskell/Server.hs
+++ server/haskell/Server.hs
@@ -2,20 +2,37 @@
 
 module Main where
 
-import Data.Text (Text)
+import Data.IORef
+import Data.Text (Text, pack)
 import Network.WebSockets
+
+type ServerState = IORef Int
 
 main :: IO ()
 main = do
   putStrLn "Listening on 127.0.0.1:9999"
-  runServer "127.0.0.1" 9999 app
 
-app :: PendingConnection -> IO ()
-app conn = do
+  countRef <- newIORef 1
+
+  runServer
+    "127.0.0.1"
+    9999
+    (\pconn -> app countRef pconn)
+
+app :: IORef Int -> PendingConnection -> IO ()
+app countRef pconn = do
   putStrLn "Client Connected"
-  putStrLn (show (pendingRequest conn))
-  connected <- acceptRequest conn
-  sendText connected "Hi"
+  putStrLn (show (pendingRequest pconn))
+
+  conn <- acceptRequest pconn
+
+  count <- readIORef countRef
+  writeIORef countRef (count + 1)
+
+  sendText conn (intToText count)
+
+intToText :: Int -> Text
+intToText n = pack (show n)
 
 sendText :: Connection -> Text -> IO ()
 sendText = sendTextData
